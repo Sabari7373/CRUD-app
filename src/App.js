d2081cc5f23b75f98b2edbd5a8e53ee362a32b14
Sabari7373 <sabariganesh7373@gmail.com>
2023-06-30 21:01:46 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Table from "./Table/Table";
+import Form from "./Form/Form";
+import { Routes, Route } from "react-router-dom";
+import EditUser from "./Edit/EditUser";
+import './404page.avif'
 
 function App() {
+  const [formList, setFormList] = useState([
+    {
+        id: 1,
+        name: "Sabari Arun",
+        email: "sabariganesh@gmail.com",
+        content: "Full Stack Devoloper",
+    },
+    {
+        id: 2,
+        name: "Bala Krishnan",
+        email: "bala@gmail.com",
+        content: "Marketing Manager",
+    },
+    {
+        id: 3,
+        name: "Ezhil Surya",
+        email: "Ezhil@gmail.com",
+        content: "IT Support",
+      },
+      {
+        id: 4,
+        name: "Dinesh",
+        email: "Dinesh@gmail.com",
+        content: "Facilitive Manager",
+      }
+  ]);
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Routes>
+      
+        <Route
+          path="/"
+          element={<Table formList={formList} setFormList={setFormList} />}
+        />
+        <Route
+          path="/form"
+          element={<Form formList={formList} setFormList={setFormList} />}
+        />
+        <Route
+          path="/edit/:id"
+          element={<EditUser formList={formList} />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="error">
+      
+      <img 
+        src='./src/404page'
+      />
     </div>
   );
 }
